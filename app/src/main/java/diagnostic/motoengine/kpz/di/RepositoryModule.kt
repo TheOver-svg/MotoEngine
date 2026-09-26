96a9cc8faa3a1d7ffb0642114c2dbcf3f584f887
TheOver-svg <turlay03102008@gmail.com>
2026-09-26 09:49:54 +0300

    created repository module

--- app/src/main/java/diagnostic/motoengine/kpz/di/RepositoryModule.kt
+++ app/src/main/java/diagnostic/motoengine/kpz/di/RepositoryModule.kt
@@ -1,4 +1,20 @@
 package diagnostic.motoengine.kpz.di
 
-class RepositoryModule {
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import diagnostic.motoengine.kpz.data.repository.DiagnosticsRepository
+import diagnostic.motoengine.kpz.data.repository.DiagnosticsRepositoryImpl
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindDiagnosticsRepository(
+        impl: DiagnosticsRepositoryImpl
+    ): DiagnosticsRepository
 }
